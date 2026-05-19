ce5ca0f6fe3535129b682c481ea1da52b547345f
JessCP25 <jpayanoc25@gmail.com>
2026-05-18 22:34:31 -0500

    feat: interoperabilidad rxjs y signals

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { CounterComponent } from '@shared/components/counter/counter.component';
 
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { FormsModule } from '@angular/forms';
 import { HighlightDirective } from '../../../shared/directives/highlight.directive';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -16,6 +18,16 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  observableWithInit$ = new BehaviorSubject<string>('initial value');
+  $withInit = toSignal(this.observableWithInit$ , {
+    requireSync: true
+  })
+
+  observableWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.observableWithoutInit$.pipe(delay(3000)), {
+    initialValue: '-----'
+  })
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -24,5 +36,13 @@ export default class AboutComponent {
   changeMessage(event: Event) {
     const input = event.target as HTMLInputElement;
     this.message.set(input.value);
+  }
+
+  emitWithInit(){
+    this.observableWithInit$.next('new value')
+  }
+
+  emitWithoutInit(){
+    this.observableWithoutInit$.next('*******')
   }
 }
